f24566ddd90e806bbb3bdd4d3cc46f3e8bf7ed37
riamf <pwl.kowalczuk@gmail.com>
2017-05-08 17:35:48 +0200

    some

---
+++ Deamon/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Deamon
+//
+//  Created by Pawel Kowalczuk on 08/05/2017.
+//  Copyright © 2017 Pawel Kowalczuk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Deamon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Deamon
+//
+//  Created by Pawel Kowalczuk on 08/05/2017.
+//  Copyright © 2017 Pawel Kowalczuk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
