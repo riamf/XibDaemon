d461f092b49890cf71997156dce3e2345c2b0afb
riamf <pwl.kowalczuk@gmail.com>
2017-05-09 19:14:05 +0200

    initial project deamon

--- Deamon/AppDelegate.swift
+++ Deamon/AppDelegate.swift
@@ -11,14 +11,41 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var statusItem: NSStatusItem!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        NSApplication.shared().windows.first?.orderOut(self)
+        
+        let statusBarItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+//        statusBarItem.menu = NSMenu()
+        statusBarItem.isVisible = true
+        statusBarItem.isEnabled = true
+        
+        statusBarItem.title = "Presses"
+        statusBarItem.action = #selector(checkApps)
+        statusBarItem.target = self
+        statusBarItem.highlightMode = true
+        statusItem = statusBarItem
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    
+    func checkApps() {
+        print("checking...")
+        NSWorkspace.shared().runningApplications.forEach { (app) in
+            let name = app.localizedName?.lowercased() ?? ""
+            print(name)
+            
+            if name == "xcode" {
+                print("znalazl sie")
+            }
+        }
+        
     }
 
 
