5e75c6cc9dc877c8fb1d8298199c14ef27fc4bf6
riamf <pwl.kowalczuk@gmail.com>
2017-05-10 19:22:20 +0200

    work in progress in retriving uielement attribut names and values

--- Deamon/AppDelegate.swift
+++ Deamon/AppDelegate.swift
@@ -7,31 +7,71 @@
 //
 
 import Cocoa
+import ApplicationServices
+import Carbon
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var statusItem: NSStatusItem!
+    var uiElement: AXUIElement!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         
         NSApplication.shared().windows.first?.orderOut(self)
         
-        let statusBarItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
-//        statusBarItem.menu = NSMenu()
-        statusBarItem.isVisible = true
-        statusBarItem.isEnabled = true
+        statusItem = addStatusBarItem()
         
-        statusBarItem.title = "Presses"
-        statusBarItem.action = #selector(checkApps)
-        statusBarItem.target = self
-        statusBarItem.highlightMode = true
-        statusItem = statusBarItem
+        askAccessibilityPermissions()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    func addStatusBarItem() -> NSStatusItem {
+        
+        let statusBarItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+        statusBarItem.isVisible = true
+        statusBarItem.isEnabled = true
+        statusBarItem.title = "Presses"
+        statusBarItem.action = #selector(checkApps)
+        statusBarItem.target = self
+        statusBarItem.highlightMode = true
+        return statusBarItem
+    }
+    
+    func askAccessibilityPermissions() {
+        
+        if !AXIsProcessTrusted() {
+            
+            let alert = NSAlert()
+            alert.addButton(withTitle: "OK")
+            alert.addButton(withTitle: "Go to Settings")
+            alert.addButton(withTitle: "Cancel")
+            alert.informativeText = "Would you like to launch System Preferences so that you can turn on \"Enable access for assistive devices\"?"
+            alert.alertStyle = .warning
+            let alertResult = alert.runModal()
+            
+            switch alertResult {
+            case NSAlertSecondButtonReturn:
+                let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.preferencePanesDirectory, FileManager.SearchPathDomainMask.systemDomainMask, true)
+                if paths.count == 1 {
+                    let url = URL(fileURLWithPath: paths.first!.appending("/UniversalAccessPref.prefPane"))
+                    NSWorkspace.shared().open(url)
+                }
+            default:
+                NSApp.terminate(self)
+                break
+            }
+        }
+        
+        uiElement = createUIElement()
+    }
+    
+    func createUIElement() -> AXUIElement {
+        return AXUIElementCreateSystemWide()
     }
     
     
@@ -39,13 +79,48 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         print("checking...")
         NSWorkspace.shared().runningApplications.forEach { (app) in
             let name = app.localizedName?.lowercased() ?? ""
-            print(name)
             
             if name == "xcode" {
-                print("znalazl sie")
+                let cocoaPoint = NSEvent.mouseLocation()
+                guard let screens = NSScreen.screens() else { return }
+                var foundScreen: NSScreen? = nil
+                for screen: NSScreen in screens {
+                    if screen.frame.contains(cocoaPoint) {
+                        foundScreen = screen
+                        break
+                    }
+                }
+                var point: CGPoint = .zero
+                if let foundScreen = foundScreen {
+                    let height = foundScreen.frame.size.height
+                    point = CGPoint(x: cocoaPoint.x, y: height - cocoaPoint.y - 1)
+                }
+                
+                var newElement: AXUIElement? = nil
+                
+                if AXUIElementCopyElementAtPosition(uiElement,
+                                                 Float(point.x),
+                                                 Float(point.y),
+                                                 &newElement) == AXError.success {
+                    
+                    guard let element = newElement else { return }
+                    
+                    var attribues = NSArray()
+                    var cfa = attribues as CFArray?
+                    
+                    AXUIElementCopyAttributeNames(element, &cfa)
+                    
+                    var actionNames = NSArray()
+                    var cfan = actionNames as CFArray?
+                    
+                    AXUIElementCopyActionNames(element, &cfan)
+                    
+                    print("something \(attribues) - \(actionNames)")
+                    
+                    
+                }
             }
         }
-        
     }
 
 
